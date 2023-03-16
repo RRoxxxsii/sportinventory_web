fb6d20916e38898f864c2da46dc9c54067f79a1c
RRoxxxsii <mishabur38@yandex.com>
2023-03-16 09:23:33 +0300

    start working with models

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,4 +1,20 @@
 from django.db import models
 
-class Category:
-    pass
+class Category(models.Model):
+    name = models.CharField(max_length=80)
+    descript = models.TextField()
+    photo =
+
+class ProductCategory(models.Model):
+    prod_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    descript = models.TextField()
+
+
+class Product(models.Model):
+    prod_name = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    prod_descript = models.TextField()
+    prod_vendor = models.CharField()
+    prod_amount = models.IntegerField(null=True)
+    price = models.DecimalField()
+
+
